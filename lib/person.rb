664d96d1e91ad829116ec2b59dd456d76cd3070c
Christopher Kim <chris.kim1990@gmail.com>
2021-02-26 17:26:54 -0500

    commit tree adb873422bc3474989e02ae54c05dc3d927892de

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,91 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def hygiene=(index)
+        @hygiene = index
+        if index > 10
+            @hygiene = 10
+        elsif index < 0
+            @hygiene = 0
+        end
+    end
+        
+    def happiness=(points)
+        @happiness = points
+        if points > 10
+            @happiness = 10
+        elsif points < 0
+            @happiness = 0
+        end
+    end
+
+   #Non-attribute instance methods
+
+   def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+    
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            friend.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            friend.happiness += 1
+            self.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+
+
+
+
+
+
+end
